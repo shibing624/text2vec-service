7aca5358c42df6a544a844b1bd82bc35812ef606
shibing624 <shibing624@126.com>
2022-05-19 11:54:17 +0800

    update http.

--- examples/http_demo.py
+++ examples/http_demo.py
@@ -9,10 +9,9 @@ import requests
 
 if __name__ == '__main__':
     ip = sys.argv[1]
-    port = int(sys.argv[2])
-    port_out = int(sys.argv[3])
+    http_port = int(sys.argv[2])
     r = requests.post(
-        f"http://{ip}:{port}/encode",
+        f"http://{ip}:{http_port}/encode",
         json={
             "id": 1,
             "texts": ["你好", "啥意思"],
