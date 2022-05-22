cefcd6879c4f05dacdb4e9b8a57a7386433aa3a1
shibing624 <shibing624@126.com>
2022-05-22 19:11:35 +0800

    update qps.

--- tests/test_qps.py
+++ tests/test_qps.py
@@ -6,6 +6,7 @@
 import os
 import time
 from loguru import logger
+import torch
 from text2vec import SentenceModel
 import sys
 
@@ -14,6 +15,9 @@ sys.path.append('..')
 logger.add('test.log')
 pwd_path = os.path.abspath(os.path.dirname(__file__))
 
+use_cuda = torch.cuda.is_available()
+repeat = 10 if use_cuda else 1
+
 
 def test_local_model_speed():
     data = ['如何更换花呗绑定银行卡',
@@ -21,13 +25,15 @@ def test_local_model_speed():
     print("data:", data)
     num_tokens = sum([len(i) for i in data])
     model = SentenceModel('shibing624/text2vec-base-chinese')
-    for j in range(10):
+    for j in range(repeat):
         tmp = data * (2 ** j)
         c_num_tokens = num_tokens * (2 ** j)
         start_t = time.time()
         r = model.encode(tmp)
         assert r is not None
         print('result shape', r.shape)
+        if j == 0:
+            logger.info(f"result shape: {r.shape}, emb: {r[0][:10]}")
         time_t = time.time() - start_t
         logger.info('encoding %d sentences, spend %.2fs, %4d samples/s, %6d tokens/s' %
                     (len(tmp), time_t, int(len(tmp) / time_t), int(c_num_tokens / time_t)))
